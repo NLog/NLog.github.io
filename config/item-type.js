0be9d84d235f340e89bb80d60a9de60c96c9943d
Julian Verdurmen <304NotModified@users.noreply.github.com>
2018-05-15 23:15:34 +0200

    Added config page with search and filters
    Added footer

---
+++ config/item-type.js
@@ -0,0 +1,36 @@
+function Item(isLayoutRenderer) {
+    this.isLayoutRenderer = isLayoutRenderer;
+}
+
+Item.prototype.packageList = function () {
+    if (!this.package) {
+        return [];
+    }
+    if (_.isArray(this.package)) {
+        return this.package;
+    }
+    return [this.package];
+}
+
+Item.prototype.href = function () {
+
+    if (!this.page) {
+        return "";
+    }
+
+    if (_.startsWith(this.page, "http://") || _.startsWith(this.page, "https://")) {
+        return this.page;
+    }
+
+    //relative URL
+    return 'https://github.com/NLog/NLog/wiki/' + this.page;
+}
+
+Item.prototype.title = function () {
+
+    if (this.isLayoutRenderer) {
+        return toLayoutName(this.name);
+    }
+
+    return this.name;
+}
