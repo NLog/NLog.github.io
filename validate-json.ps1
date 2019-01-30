3facf3a50545b697b9b904cdbe5c2baf8836dc07
Julian Verdurmen <304NotModified@users.noreply.github.com>
2019-01-30 21:57:57 +0100

    error report 2

--- validate-json.ps1
+++ validate-json.ps1
@@ -2,11 +2,17 @@ npm install jsonlint -g
 
 Write-Host "Validate targets.json ..."
 jsonlint config/targets.json -q  2>&1
+if (-Not $LastExitCode -eq 0)
+	{ exit $LastExitCode }
 
 Write-Host "Validate layouts.json ..."
 jsonlint config/layouts.json -q  2>&1
+if (-Not $LastExitCode -eq 0)
+	{ exit $LastExitCode }
 
 Write-Host "Validate layout-renderers.json ..."
 jsonlint config/layout-renderers.json -q  2>&1
+if (-Not $LastExitCode -eq 0)
+	{ exit $LastExitCode }
 
 Write-Host "JSON Validation done"
