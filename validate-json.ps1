1bbc708755d45499eef56f7a043ef5f557528802
Julian Verdurmen <304NotModified@users.noreply.github.com>
2019-12-13 22:47:45 +0100

    Json schema for targets v1 (#138)
    * Json schema for targets
    
    * write output

--- validate-json.ps1
+++ validate-json.ps1
@@ -1,18 +1,18 @@
-npm install jsonlint -g
+npm install @prantlf/jsonlint -g
 
-Write-Host "Validate targets.json ..."
-jsonlint config/targets.json -q  2>&1
+Write-Output "Validate targets.json ..."
+jsonlint config/targets.json --quiet --no-duplicate-keys --validate config/targets.schema.json --environment json-schema-draft-07 2>&1
 if (-Not $LastExitCode -eq 0)
 	{ exit $LastExitCode }
 
-Write-Host "Validate layouts.json ..."
+Write-Output "Validate layouts.json ..."
 jsonlint config/layouts.json -q  2>&1
 if (-Not $LastExitCode -eq 0)
 	{ exit $LastExitCode }
 
-Write-Host "Validate layout-renderers.json ..."
+Write-Output "Validate layout-renderers.json ..."
 jsonlint config/layout-renderers.json -q  2>&1
 if (-Not $LastExitCode -eq 0)
 	{ exit $LastExitCode }
 
-Write-Host "JSON Validation done"
+Write-Output "JSON Validation done"
